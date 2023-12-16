af6229843ff1246cfcdc917091ad0f393ccace59
mohammad hashemy <smhd.hashemy@gmail.com>
2023-12-16 15:59:10 +0330

    up

--- Planners/ForwardPlanner_with_h.py
+++ Planners/ForwardPlanner_with_h.py
@@ -18,34 +18,7 @@ class ForwardPlanner(Planner):
         super().__init__(problem)
 
     def search(self):
-        frontier = PriorityQueue()
-        visited = set()
-        goal = self.problem.goal_state
-
-        # Check if goal state is already reached
-        if self.problem.initial_state.goal_test(goal):
-            return []
-
-        # Add initial state to frontier and visited set
-        frontier.put((0, self.problem.initial_state))
-        visited.add(self.problem.initial_state)
-        while not frontier.empty():
-            current_state = frontier.get()[1]
-
-            # Generate successor states
-            successor_states = self.successor(current_state)
-
-            for successor_state in successor_states:
-                # If goal state is found, build solution and return
-                if successor_state.goal_test(goal):
-                    return successor_state.build_solution()
-
-                # Add successor state to frontier and visited set
-                if successor_state not in visited:
-                    h = self.idl_heuristic(successor_state)
-                    print(h)
-                    frontier.put((h, successor_state))
-                    visited.add(successor_state)
+        ...
 
         return []
 
@@ -63,22 +36,3 @@ class ForwardPlanner(Planner):
 
     def idl_heuristic(self, state) -> int:
         return 10000
-
-    def get_result_len(self, successor_state, state):
-        sum = 0
-        while successor_state != state:
-            sum += 1
-            successor_state = successor_state.parent
-        return sum
-
-    def idl_successor(self, current_state):
-        result = []
-        for action in self.problem.domain.actions:
-            # Check if action is applicable to state
-            if action.is_applicable(current_state):
-                new_state = action.progress_without_delete(current_state)
-                # Set parent state and action
-                new_state.parent = current_state
-                result.append(new_state)
-
-        return result
